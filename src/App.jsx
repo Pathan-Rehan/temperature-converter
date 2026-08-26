836e065f92df01cd295201a45a4cbed3f451e191
Pathan Rehan <pathanrehankhan68@gmail.com>
2026-08-26 16:43:14 +0530

    feat: style converter UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-
+import TemperatureConverter from './components/TemperatureConverter'
+import   './App.css'
 function App() {
 
 
   return (
     <div>
-      <h1>Temperature Converter</h1>
+      <TemperatureConverter />
     </div>
   )
 }
--- src/components/TemperatureConverter.jsx
+++ src/components/TemperatureConverter.jsx
@@ -1,4 +1,33 @@
-export const TemperatureConverter = () => {
- return <div></div>;
+export default function TemperatureConverter() {
+     return <div className="converter">
+          <h1 >Temperature Converter</h1>
+
+          <input
+               type="number"
+               placeholder="Enter temperature"
+          />
+
+          <select>
+               <option value="C">Celsius</option>
+               <option value="F">Fahrenheit</option>
+               <option value="K">Kelvin</option>
+          </select>
+
+          <select>
+               <option value="C">Celsius</option>
+               <option value="F">Fahrenheit</option>
+               <option value="K">Kelvin</option>
+          </select>
+
+          <button>Convert</button>
+
+          <div className="result">
+               <p>Result will appear here</p>
+          </div>
+
+          <div className="error">
+               <p>Error will appear here</p>
+          </div>
+     </div>;
 
 }
