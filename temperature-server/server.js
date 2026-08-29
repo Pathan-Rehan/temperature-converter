adf740311536971134a0229a605bc76b9e368b27
Pathan Rehan <pathanrehankhan68@gmail.com>
2026-08-29 16:55:52 +0530

    feat: add basic error handling

--- temperature-server/server.js
+++ temperature-server/server.js
@@ -11,6 +11,13 @@ app.get("/", (req, res) => {
   res.send("Temperature Converter API is running");
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    error: 'Internal server error',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
